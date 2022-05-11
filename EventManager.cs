3603a0cae99653b4c22c23e8ac441b76e4b7bdbd
Win7yes <7284418+francodml@users.noreply.github.com>
2022-05-11 16:16:52 -0300

    Stub out EventManager

---
+++ EventManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eden
+{
+    public class EventManager
+    {
+
+        public static EventManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new EventManager();
+                return instance;
+            }
+        }
+
+        private static EventManager? instance;
+        
+        private EventManager() { }
+
+
+    }
+}
